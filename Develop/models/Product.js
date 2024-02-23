63b6bdfaa3c2b86fe10e12b7d4c6fae5f87f5006
Jose Silva <js38902@gmail.com>
2024-02-22 22:21:31 -0500

    Finished Models and requirments

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,14 +9,49 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    //Defining the columns (id, product_name, price etc.) with their requirments
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        //custom validation here, validating that the value is a decimal
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        //validating that the value is a numeric value
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      //referencing the category table/model
+      references: {
+        model: "Category",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
